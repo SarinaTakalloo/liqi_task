14d4db3ae9d967be2da96e5d78bf43f789fa66f0
Sarina Takalloo <sarina.takalloo@studenti.polito.it>
2023-03-15 16:15:06 +0100

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -37,7 +37,8 @@ df['in-flow'] = df[df['amount'] > 0]['amount']
 df['out-flow'] = df[df['amount'] < 0]['amount']
 
 
-##in this task there is no need to calculate the average monthly cash flow however, to have a better insight of what's going on every month, I made this part available 
+##in this task there is no need to calculate the average monthly cash flow. 
+##however, to have a better insight of what's going on every month, I made this part available 
 #df['month'] = df['data'].dt.month
 #monthly = df.groupby('month').agg({'in-flow': "sum", 'out-flow': "sum"})
 
@@ -49,14 +50,28 @@ df['out-flow'] = df[df['amount'] < 0]['amount']
 #monthly['Cash Balance'] = monthly['in-flow'] + monthly['out-flow']
 #max_cash_balance = monthly['Cash Balance'].max()
 
+
+
+
+
 #calculate all the positive amount and negative amount as current assets and liabilities/debts respectively and the obtaining current available cash
 in_flow , out_flow = df.agg({'in-flow': "sum", 'out-flow': "sum"})
 current_cash = in_flow - abs(out_flow)
 
-#I have considered the currents assets as INCASSO FATTURA and EROGAZIONE FINANZIAMENTO; but, in reality to determine a correct assets, 
-#other data is needed such as cash, cash equivalents, short-term investments, accounts receivable, inventory, supplies, and prepaid expenses.
+#I have considered the current assets as INCASSO FATTURA and EROGAZIONE FINANZIAMENTO; but, in reality to determine correct current assets, 
+#other type of data is needed such as cash, cash equivalents, short-term investments, accounts receivable, inventory, supplies, and prepaid expenses.
 #in this task there are only funding distributions and invoice collection
-current_assets = df.loc[df['categories'].isin(['INCASSO FATTURA','EROGAZIONE FINANZIAMENTO']), 'amount'].sum()
+
+total_revenue = df.loc[df['categories'].isin(['INCASSO FATTURA','EROGAZIONE FINANZIAMENTO']), 'amount'].sum()
+operating_expenses = df.loc[df['categories'].isin(['STIPENDI', 'UTENZE' , 'PAGAMENTO FORNITORI','PAGAMENTO FATTURA']), 'amount'].sum()
+debt = df.loc[df['categories'].isin(['RIMBORSO FINANZIAMENTO']), 'amount'].sum()
+
+#calculate different ratios:
+
+debt_to_liability_ratio = debt/operating_expenses
+debt_service_coverage_ratio = (total_revenue - operating_expenses)/debt
+current_ratio = total_revenue/(debt + operating_expenses)
+
 
 
 
